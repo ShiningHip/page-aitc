015335154809dd4c0b79d21ef770c2732faf892d
ShiningHip <shininghip.niigata@gmail.com>
2020-02-24 22:31:09 +0900

    redirect to page from top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Context } from 'vm'
 
-const Index: React.FC = () => <></>
+const Index = () => <></>
+Index.getInitialProps = async ({ res }: Context) => {
+  if (res) {
+    res.writeHead(302, { Location: '/product/AITC-0001' })
+    res.end()
+  }
+}
 export default Index
